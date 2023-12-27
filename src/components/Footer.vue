daf99f457fd52da3dc57314a47e486acee084ac9
ArtemPedenko <nubostrel@gmail.com>
2023-12-27 18:51:56 +0300

    test deploy

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <footer
-    class="bg-black text-white max-w-[1920px] h-[50px] flex justify-center items-center"
-  >
-    Made not on tilda
-  </footer>
-</template>
