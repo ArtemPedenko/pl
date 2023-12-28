db8b26c17cd629ee588e8464a51700156eb05789
Artem Pedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-12-28 07:41:55 +0000

    done friend section for mobile

--- src/sections/FriendSection.vue
+++ src/sections/FriendSection.vue
@@ -7,9 +7,11 @@
     <div
       class="absolute -z-10 w-full h-full bg-[url('@/assets/friend.png')] bg-cover filter brightness-[40%] bg-center"
     ></div>
-    <div class="z-20 flex flex-col max-w-[640px] gap-[36px] text-center">
-      <p class="text-[67px] font-bold">ПРИВЕДИ ДРУГА!</p>
-      <p class="text-2xl font-normal">
+    <div
+      class="z-20 flex flex-col max-w-[640px] gap-[36px] text-center max-xl:mx-[20px]"
+    >
+      <p class="text-[67px] font-bold max-md:text-[50px]">ПРИВЕДИ ДРУГА!</p>
+      <p class="text-[24px] font-normal max-md:text-[18px]">
         Мы будем рады, если ты будешь рекомендовать работу в нашей компании
         своим друзьям/коллегам/родственникам. Если твоего друга или знакомого
         берут к нам на склад, и он успешно проходит испытательный срок, то ты
