ce023fa7390de9918c0ee9ae443122a28f144393
Artem Pedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-12-28 07:58:01 +0000

    done registration section for mobile

--- src/sections/RegistrationSection.vue
+++ src/sections/RegistrationSection.vue
@@ -10,23 +10,27 @@ import Button from "@/components/Button.vue";
     <div
       class="z-20 flex max-w-[1200px] max-h-[540px] w-full h-screen bg-[#E3E9ED] rounded-[25px] overflow-hidden"
     >
-      <div class="flex flex-col gap-3 w-1/2 py-[60px] pl-[60px] pr-[80px]">
-        <p class="text-[38px] font-bold">
+      <div
+        class="flex flex-col gap-3 w-1/2 max-lg:w-full max-lg:justify-center max-lg:items-center max-lg:text-center py-[60px] pl-[60px] pr-[80px]"
+      >
+        <p class="text-[38px] font-bold max-xl:text-[30px]">
           Не упустите шанс подняться по карьерной лестнице!
         </p>
         <div class="w-full border border-[#0EBC05]"></div>
-        <p class="text-[20px]">
+        <p class="text-[20px] max-xl:text-[18px]">
           Зарегистрируйтесь и позвольте нашим менеджерам предложить вам самые
           подходящие вакансии.
         </p>
-        <div class="w-[280px] h-[60px] text-[18px] text-white mt-[50px]">
+        <div
+          class="max-w-[280px] w-full h-[60px] mt-[50px] max-lg:mt-[10px] text-[18px] text-white"
+        >
           <Button href="/registration">Пройти регистрацию</Button>
         </div>
       </div>
       <img
         alt="register girl"
         src="@/assets/register-girl.png"
-        class="h-full w-1/2 object-cover"
+        class="h-full w-1/2 object-cover max-lg:hidden"
       />
     </div>
   </div>
